96440acc937a21a51f861d90c029134f76adbaa0
Mateus <mateusvg-99@hotmail.com>
2023-01-23 17:27:10 -0300

    Adapted gen_x_and_y to return the codec name - Removed unused datasets

--- main.py
+++ main.py
@@ -9,28 +9,13 @@ from src.model import DISTS
 
 DBS_METADATA = {
     'APSIPA': {
-        'ref_base': r'./APSIPA_projections/ref_projections/',
-        'deg_base': r'./APSIPA_projections/deg_projections/',
-        'df_path': r'./APSIPA.csv'
-    },
-    'QOMEX': {
-        'ref_base': r'./qomex_projections/ref_projections/',
-        'deg_base': r'./qomex_projections/deg_projections/',
-        'df_path': r'./QOMEX.csv'
+        'ref_base': '/mnt/d/Arquivos/Desktop/APSIPA___M-PCCD/ref_projections/',
+        'deg_base': '/mnt/d/Arquivos/Desktop/APSIPA___M-PCCD/deg_projections/',
+        'df_path': '/mnt/d/Arquivos/Desktop/APSIPA___M-PCCD/APSIPA.csv'
     },
     'WPC': {
         'ref_base': r'./wpc_projections/ref_projections/',
         'deg_base': r'./wpc_projections/deg_projections/',
-        'df_path': r'./WPC.csv'
-    },
-    'UNB-PCQA': {
-        'ref_base': r'./unb_projections/ref_projections/',
-        'deg_base': r'./unb_projections/deg_projections/',
-        'df_path': r'./WPC.csv'
-    },
-    'SJTU': {
-        'ref_base': r'./sjtu_projections/ref_projections/',
-        'deg_base': r'./sjtu_projections/deg_projections/',
         'df_path': r'./WPC.csv'
     }
 }
@@ -39,7 +24,7 @@ DBS_METADATA = {
 def gen_x_and_y(
     db_metadata: dict[str, str],
     model: DISTS
-) -> tuple[list[list[float]], list[float], list[str]]:
+) -> tuple[list[list[float]], list[float], list[str], list[str]]:
     df_data = pd.read_csv(db_metadata['df_path'])
 
     generator = LoadProjectionsData(
@@ -49,7 +34,7 @@ def gen_x_and_y(
     )
 
     device = torch.device(
-        "mps") if torch.backends.mps.is_available() else torch.device("cpu")
+        "mps") if torch.backends.mps.is_available() else torch.device("cpu") # type: ignore
     model.to(device)
     print(f"model is using {device}")
     data_gen = generator.data_generator()
@@ -60,13 +45,16 @@ def gen_x_and_y(
         y.append(pair_proj.score)
         ref_names.append(pair_proj.ref.name)
 
-    return X, y, ref_names
+    codecs = list(df_data['ATTACK'])
+
+    return X, y, ref_names, codecs
 
 
 if __name__ == '__main__':
     for db in DBS_METADATA.keys():
         model = DISTS()
-        X, y, ref_names = gen_x_and_y(DBS_METADATA[db], model)
-        torch.save(X, f'X_tensor_{db}.pt')
-        torch.save(y, f'y_tensor_{db}.pt')
-        joblib.dump(ref_names, f'ref_names_{db}.pkl')
+        X, y, ref_names, codecs = gen_x_and_y(DBS_METADATA[db], model)
+        torch.save(X, f'./data/X_tensor_{db}.pt')
+        torch.save(y, f'./data/y_tensor_{db}.pt')
+        joblib.dump(ref_names, f'./data/ref_names_{db}.pkl')
+        joblib.dump(codecs, f'./data/codecs_{db}.pkl')
