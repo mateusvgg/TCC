f8db194b683b913f16c8b57160c2dea1db4f1027
Johann Homonnai <johannhomonnai@gmail.com>
2023-01-28 15:37:00 -0300

    adapted gen_x_and_y to include WPC's degradation

--- main.py
+++ main.py
@@ -14,16 +14,17 @@ DBS_METADATA = {
         'df_path': '/Volumes/mnt/APSIPA_projections/APSIPA.csv'
     },
     'WPC': {
-        'ref_base': r'./wpc_projections/ref_projections/',
-        'deg_base': r'./wpc_projections/deg_projections/',
-        'df_path': r'./WPC.csv'
+        'ref_base': r'/Volumes/mnt/WPC/ref_projections/',
+        'deg_base': r'/Volumes/mnt/WPC/deg_projections/',
+        'df_path': r'/Volumes/mnt/WPC/WPC_BACKUP.csv'
     }
 }
 
 
 def gen_x_and_y(
     db_metadata: dict[str, str],
-    model: DISTS
+    model: DISTS,
+    db_name: str
 ) -> tuple[list[list[float]], list[float], list[str], list[str]]:
     df_data = pd.read_csv(db_metadata['df_path'])
 
@@ -34,7 +35,7 @@ def gen_x_and_y(
     )
 
     device = torch.device(
-        "mps") if torch.backends.mps.is_available() else torch.device("cpu")  # type: ignore
+        "mps") if torch.backends.mps.is_available() else torch.device("cpu")
     model.to(device)
     print(f"model is using {device}")
     data_gen = generator.data_generator()
@@ -45,16 +46,19 @@ def gen_x_and_y(
         y.append(pair_proj.score)
         ref_names.append(pair_proj.ref.name)
 
-    codecs = list(df_data['ATTACK'])
+    if db_name == 'WPC':
+        degradations = list(df_data['Distortion_Type'])
+    elif db_name == 'APSIPA':
+        degradations = list(df_data['ATTACK'])
 
-    return X, y, ref_names, codecs
+    return X, y, ref_names, degradations
 
 
 if __name__ == '__main__':
     for db in DBS_METADATA.keys():
         model = DISTS()
-        X, y, ref_names, codecs = gen_x_and_y(DBS_METADATA[db], model)
+        X, y, ref_names, codecs = gen_x_and_y(DBS_METADATA[db], model, db)
         torch.save(X, f'./data/X_tensor_{db}.pt')
         torch.save(y, f'./data/y_tensor_{db}.pt')
         joblib.dump(ref_names, f'./data/ref_names_{db}.pkl')
-        joblib.dump(codecs, f'./data/codecs_{db}.pkl')
+        joblib.dump(codecs, f'./data/degradations_{db}.pkl')
