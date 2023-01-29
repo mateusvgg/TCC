844f151bd46939ae4827dae0a4b6baf073e81551
Johann Homonnai <johannhomonnai@gmail.com>
2023-01-29 19:02:09 -0300

    reduced test set size, reran lazypredict

--- perceptual_driven_pcqm/lazypredict_projections.py
+++ perceptual_driven_pcqm/lazypredict_projections.py
@@ -25,7 +25,7 @@ def load_and_preprocess_data(dataset: str) -> ModelData:
     y = np.array(y)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.5, random_state=42
+        X, y, test_size=0.25, random_state=42
     )
 
     return X_train, X_test, y_train, y_test
